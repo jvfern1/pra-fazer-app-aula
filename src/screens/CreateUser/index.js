7bae7dc639e1c93da538e47c43f0a0b105cf787a
Edemilton Junior <edemiltonjr@gmail.com>
2024-04-29 22:50:28 -0300

    Ajustes no Buttom Tab e validação do cadastro de usuários

--- src/screens/CreateUser/index.js
+++ src/screens/CreateUser/index.js
@@ -1,9 +1,24 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './style'
 
 export default function CreateUser() {
-    let errorCreateUser = null
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const [errorCreateUser, setErrorCreateUser] = useState(null)
+
+    function validate(){
+        if (nome == ""){
+            setErrorCreateUser("informe o seu nome")
+        } else if (email == "") {
+            setErrorCreateUser("Informe o seu e-mail")
+        } else if (senha == "") {
+            setErrorCreateUser("Informe uma senha");
+        } else {
+            setErrorCreateUser(null)
+        }
+    }
 
     return (
         <View style={styles.container}>
@@ -14,21 +29,28 @@ export default function CreateUser() {
             <TextInput
                 style={styles.input}
                 placeholder='Nome'
+                value={nome}
+                onChangeText={setNome}
             />
 
             <TextInput
                 style={styles.input}
                 placeholder='E-mail'
+                value={email}
+                onChangeText={setEmail}
             />
 
             <TextInput
                 style={styles.input}
                 secureTextEntry={true}
                 placeholder='Senha'
+                value={senha}
+                onChangeText={setSenha}
             />
 
             <TouchableOpacity
                 style={styles.button}
+                onPress={validate}
             >
                 <Text style={styles.textButton}>Criar usuário</Text>
             </TouchableOpacity>
